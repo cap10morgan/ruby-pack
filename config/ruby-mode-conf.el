3c2ab8d7eabd9fd7348da435c9d3461e8a364fa6
Wes Morgan <cap10morgan@gmail.com>
2013-04-25 20:54:18 -0600

    add additional file types to ruby-mode

---
+++ config/ruby-mode-conf.el
@@ -0,0 +1,18 @@
+;; Add additional file types to trigger ruby-mode
+(defun add-to-ruby-mode (file-re)
+  (add-to-list 'auto-mode-alist (cons file-re 'ruby-mode)))
+
+(defvar ruby-mode-files
+  '("\\.rake\\'"
+    "Rakefile\\'"
+    "\\.gemspec\\'"
+    "\\.ru\\'"
+    "Gemfile\\'"
+    "Guardfile\\'"
+    "Capfile\\'"
+    "\\.thor\\'"
+    "Thorfile\\'"
+    "Vagrantfile\\'"
+    "\\.jbuilder\\'"))
+
+(mapcar 'add-to-ruby-mode ruby-mode-files)
--- init.el
+++ init.el
@@ -1,2 +1,3 @@
+(live-load-config-file "ruby-mode-conf.el")
 (live-load-config-file "autopair-conf.el")
 (live-load-config-file "ruby-end-conf.el")
