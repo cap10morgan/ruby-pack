cd687f37c0b0f6caf5dc81cc550d18fc38f1f802
Wes Morgan <cap10morgan@gmail.com>
2013-05-15 11:32:08 -0600

    attempt to fix min. versions

--- ruby-pack-pkg.el
+++ ruby-pack-pkg.el
@@ -1,3 +1,3 @@
 (define-package "ruby-pack" "0.1.0"
   "Emacs Live Pack for better Ruby support."
-  '((autopair "") (ruby-end "0.2.2") (ruby-tools "")))
+  '((autopair "0") (ruby-end "0.2.2") (ruby-tools "0")))
