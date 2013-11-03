bf60fca6dd161603c1ef8cee9c84922b8d6a84ef
Wes Morgan <cap10morgan@gmail.com>
2013-11-03 05:04:58 -0700

    add web-mode-hook hook to disable whitespace-mode since it's incompatible with web-mode

--- config/webmode-conf.el
+++ config/webmode-conf.el
@@ -7,3 +7,6 @@
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+
+;; disable whitespace-mode in web-mode because they're incompatible
+(add-hook 'web-mode-hook (lambda () (whitespace-mode -1)))
